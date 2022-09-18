8cf2d09ebd90b442d21e39919b109a3121a66c7d
haha <390519764@qq.com>
2022-09-18 21:40:48 +0800

    feat: finsh h function

--- src/h.js
+++ src/h.js
@@ -1,4 +1,24 @@
+import { createVNode, isVNode } from "./vnode"
 // createNode的语法糖
-const h = (type, propsOrChildren, children) => {
-    
+export function h(type, propsOrChildren, children) {
+  const l = arguments.length
+  if (l === 2) {
+    if (isObject(propsOrChildren) && !isArray(propsOrChildren)) {
+      // 简单vnode没有props
+      if (isVNode) {
+        return createVNode(type, null, [propsOrChildren])
+      }
+      // only have props
+      return createVNode(type, propsOrChildren)
+    } else {
+      return createVNode(type, null, propsOrChildren)
+    }
+  } else {
+    if (l > 3) {
+      children = Array.prototype.slice(arguments, 2)
+    } else if (l === 3 && isVNode(children)) {
+      children = [children]
+    }
+    return createVNode(type, propsOrChildren, children)
+  }
 }
